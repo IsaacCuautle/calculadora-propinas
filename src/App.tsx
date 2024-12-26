66b9f5b57dbee4d4d28e2c80dfec4394cb64113c
Luis Isaac Cuautle Delgadillo <cuautle.tron@gmail.com>
2024-12-26 11:08:39 -0600

    añadir articulos a la orden

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-function App() {
-  
+import MenuItem from "./components/MenuItem";
+import { menuItems } from "./data/db";
+import useOrder from "./hooks/useOrder";
+
+function App() {  
+
+  const {addItem} = useOrder();
 
   return (
     <>
@@ -15,8 +20,57 @@ function App() {
             text-4xl
             font-black
           "
-        >Calculadora de Propinas y consumos</h1>
+        >
+          Calculadora de Propinas y consumos
+        </h1>
       </header>
+
+      <main
+        className="
+          max-w-7xl
+          mx-auto
+          py-20
+          // Crea 2 columnas 
+          grid 
+          md:grid-cols-2
+        "
+      >
+
+        <div
+          className="
+            p-5
+          "
+        >
+          <h2
+            className="
+              font-black
+              text-4xl
+            "
+          >
+            Menú
+          </h2>
+            <div
+              className="
+                space-y-3
+                mt-10
+              ">
+              {menuItems.map( item => (
+                
+                <MenuItem
+                  key={item.id}
+                  item ={item}
+                  addItem={addItem}
+                />
+
+              ))}
+            </div>
+        </div>
+
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      
+      </main>
     </>
   )
 }
